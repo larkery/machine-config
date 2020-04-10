a68e28cc9d383ba3443657e4afdcc1994a2624f2
Tom Hinton <tom.hinton@cse.org.uk>
2020-04-10 15:00:51 +0100

    Add teams; add hack to use my xdg-utils without full rebuild

--- packages.nix
+++ packages.nix
@@ -3,20 +3,20 @@
   nixpkgs.config.allowUnfree = true;
 
   nixpkgs.overlays = [
-  (self : super : {
-    emacs = super.emacs.override {
-       withGTK2 = false;
-       withGTK3 = false;
-       imagemagick = self.imagemagick;
-    };
+    (self : super : {
+      emacs = super.emacs.override {
+        withGTK2 = false;
+        withGTK3 = false;
+        imagemagick = self.imagemagick;
+      };
 
-    dmenu = super.dmenu.overrideAttrs (a : {
-       patches = [./patches/dmenu-number-output.patch];
-    });
+      dmenu = super.dmenu.overrideAttrs (a : {
+        patches = [./patches/dmenu-number-output.patch];
+      });
 
-    pass = super.pass.override {gnupg = self.gnupg;};
-    srandrd = self.callPackage ./srandrd.nix {};
-  })
+      pass = super.pass.override {gnupg = self.gnupg;};
+      srandrd = self.callPackage ./srandrd.nix {};
+    })
   ];
   
   environment.systemPackages =
@@ -24,6 +24,33 @@
     mkEmacs = (pkgs.emacsPackagesNgGen pkgs.emacs).emacsWithPackages;
     myEmacs = mkEmacs
     (e: [ e.pdf-tools pkgs.notmuch e.org ] );
+
+
+    fixed-xdg-utils = derivation {
+      name = pkgs.xdg_utils.name;
+      version = pkgs.xdg_utils.version;
+      system = builtins.currentSystem;
+      builder = let xo2 = pkgs.writeScript "xdg-open" ''
+        #! ${pkgs.bash}/bin/bash
+        exec ''${HOME}/bin/xdg-open "$@"
+      '';
+      in
+      pkgs.writeScript "build" ''
+        #! ${pkgs.bash}/bin/bash
+        ${pkgs.coreutils}/bin/mkdir $out
+        ${pkgs.coreutils}/bin/ln -s ${pkgs.xdg_utils}/* $out
+        ${pkgs.coreutils}/bin/rm -rf $out/bin
+        ${pkgs.coreutils}/bin/mkdir -p $out/bin
+        ${pkgs.coreutils}/bin/ln -s ${pkgs.xdg_utils}/bin/* $out/bin
+        ${pkgs.coreutils}/bin/ln -sf ${xo2} $out/bin/xdg-open
+      '';
+    };
+    
+    replace-xdg = pkgs.writeScriptBin "replace-xdg" ''
+      #! ${pkgs.bash}/bin/bash
+      
+      exec ${pkgs.bubblewrap}/bin/bwrap --dev-bind / / --bind ${fixed-xdg-utils} ${pkgs.xdg_utils} "$@"
+    '';
   in
   with pkgs; [
     gnome3.defaultIconTheme
@@ -64,8 +91,15 @@
     compton
     libnotify
 
-    firefox chromium
+    firefox
+    chromium
     pinentry
+
+    tdesktop
+
+    replace-xdg
+    
+    (callPackage ./ms-teams.nix {})
   ];
 
 }
