87ef3dc2f58bd03486fb37db1449ce4f709f9c6f
Tom Hinton <tom.hinton@cse.org.uk>
2022-09-22 14:40:51 +0100

    Not better

--- device/larkery.com.nix
+++ device/larkery.com.nix
@@ -49,7 +49,7 @@
     
     serviceConfig = {
       User="hinton";
-      ExecStart = "${pkgs.gotty}/bin/gotty -w -a 127.0.0.1 /run/current-system/sw/bin/zsh";
+      ExecStart = "${pkgs.gotty}/bin/gotty -w -a 127.0.0.1 /run/current-system/sw/bin/login";
     };
   };
   
